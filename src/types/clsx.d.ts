110d1e3182afe8db2ed00b9ed582d81bc54db883
lestersinjacreater <lestetsinjawatosia@gmail.com>
2025-04-17 16:37:51 +0300

    authentication fix

---
+++ src/types/clsx.d.ts
@@ -0,0 +1,5 @@
+// filepath: c:\Users\User\Desktop\software developmennt projects\deadman\studio\src\types\clsx.d.ts
+declare module "clsx" {
+    export function clsx(...inputs: any[]): string;
+    export type ClassValue = string | number | null | undefined | { [key: string]: boolean };
+  }
